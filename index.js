a6e81e0f12a8eb16cff14acf3f212491ad108ded
fantasyui-com <captain@fantasyui.com>
2018-06-08 12:18:40 -0400

    hex/rgb/rgba support

--- index.js
+++ index.js
@@ -8,37 +8,35 @@ const fs = require('fs');
 
 module.exports = {
   importData: function({location}){
+    const npm = JSON.parse(fs.readFileSync(path.join('.','package.json')).toString());
 
     const output = {
       "meta": {
-        "name": "untitled"
+        "name": npm.name
       },
       "data": []
     };
 
     const list = JSON.parse(fs.readFileSync(location).toString());
-
     list.forEach(function(item, index){
       if (index > 25) return;
-
       const id = String.fromCharCode(97+index);
+      const gradients = [];
+
       const type = "linear-gradient";
       const angle = "0deg";
       const data = [];
-      const gradients = [];
+
       const gradient = {type, angle, data};
 
-
       gradients.push(gradient);
-      const myRegExp = new RegExp("(#[a-f0-9]{6}) ([0-9]+%)",'gi');
+      const myRegExp = new RegExp("(#[a-f0-9]{6}|rgba\\([0-9.]+ *, *[0-9.]+ *, *[0-9.]+ *, *[0-9.]+ *\\)|rgb\\([0-9.]+ *, *[0-9.]+ *, *[0-9.]+ *\\)) ([0-9]+%)",'gi');
       let myArray;
       while (( myArray = myRegExp.exec(item)) !== null) {
         const [, color, position] = myArray;
-        console.log(color, position);
         data.push({color, position})
       }
       output.data.push({id, gradients})
-
     });
 
     console.log( JSON.stringify(output, null, '  ') );
